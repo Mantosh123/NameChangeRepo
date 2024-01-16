02b243e07241727773c76b821850c42def85a947
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-01-16 22:15:41 +0530

    Initial commit with text change

---
+++ NameChangeDemo/NameChangeDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NameChangeDemo
+//
+//  Created by Mantosh Kumar on 16/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        print("Repo Name Change demo ")
+    }
+
+
+}
+
